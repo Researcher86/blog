ee8bcfd5cf45117319c725357fc67d5b35e1b957
Tanat <researcher2286@gmail.com>
2019-06-09 11:49:28 +0600

    Refactoring application layer

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -1,7 +1,5 @@
 <?php
 
-use Blog\Application\Service\ApplicationService;
-use Blog\Application\Service\User\GetAllUsersService;
 use Blog\Domain\Model\User\UserRepository;
 use Blog\Infrastructure\Persistence\Doctrine\Domain\Model\User\DoctrineUserRepository;
 
@@ -13,7 +11,6 @@ return [
 
         'aliases' => [
             UserRepository::class => DoctrineUserRepository::class,
-            ApplicationService::class => GetAllUsersService::class,
         ],
     ],
 
--- src/Infrastructure/UI/Web/Handler/GetAllUsersHandler.php
+++ src/Infrastructure/UI/Web/Handler/GetAllUsersHandler.php
@@ -2,7 +2,7 @@
 
 namespace Blog\Infrastructure\UI\Web\Handler;
 
-use Blog\Application\Service\ApplicationService;
+use Blog\Application\Service\User\UserService;
 use Blog\Infrastructure\UI\Web\Template\ViewResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,23 +11,23 @@ use Psr\Http\Server\RequestHandlerInterface;
 class GetAllUsersHandler implements RequestHandlerInterface
 {
     /**
-     * @var ApplicationService
+     * @var UserService
      */
     private $service;
 
 
     /**
      * GetAllUsersHandler constructor.
-     * @param ApplicationService $service
+     * @param UserService $service
      */
-    public function __construct(ApplicationService $service)
+    public function __construct(UserService $service)
     {
         $this->service = $service;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $users = $this->service->execute($request);
+        $users = $this->service->getAll($request);
 
         return new ViewResponse('users', ['users' => $users]);
     }
